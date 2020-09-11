a0744866f413ed39d6720fb4fef9bb4ff31de4fd
Carlos Cartagena-Sanchez <ccartagena@brynmawr.edu>
2020-09-11 12:20:46 -0500

    Small step towards class

---
+++ client.py
@@ -0,0 +1,18 @@
+#############################
+"""
+    Client Class
+"""
+#############################
+
+class Client:
+    """
+        Client class
+    """
+    def __init__(self, income, file_status):
+        self.income = income
+        self.file_status = file_status
+    
+        
+
+    
+    
